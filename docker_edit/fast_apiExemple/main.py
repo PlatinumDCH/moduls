7f9f4d2eb86c323af0ecdbecf521f7acb23f73c0
dima cheban <dimacheban23@gmail.com>
2025-02-03 21:47:35 +0200

    added exemples Docker

---
+++ docker_edit/fast_apiExemple/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/test")
+async def root():
+    return {"message": "testing message"}
+
