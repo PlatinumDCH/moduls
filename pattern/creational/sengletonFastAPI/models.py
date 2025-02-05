2c6eaf1dfef165afa0b38827de6e9b5a141ba2f0
dima cheban <dimacheban23@gmail.com>
2025-02-05 11:15:45 +0200

    add pattern singleton

---
+++ pattern/creational/sengletonFastAPI/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from pattern.creational.sengletonFastAPI.database import base
+
+class Todo(base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=False)
+    completed = Column(Boolean, default=False)
+
+
---
+++ pattern/creational/sengletonFastAPI/register.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, APIRouter
+from pattern.creational.sengletonFastAPI.routes import router as import_router
+
+class APIRouterManager:
+    _routes:list = []
+
+    @classmethod
+    def register(cls, router: APIRouter):
+        cls._routes.append(router)
+    
+    @classmethod
+    def attach(cls, app: FastAPI):
+        for router in cls._routes:
+            app.include_router(router)
+
+
+
+APIRouterManager.register(import_router)
+
+
+
